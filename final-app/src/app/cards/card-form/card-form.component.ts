0cd6558392d755de0b397d22f568e0c5227e1c5d
Cpatrici23 <claudia.munozgarcia@gmail.com>
2023-05-21 12:58:39 +0200

    Cambios en card-form.component

--- final-app/src/app/cards/card-form/card-form.component.ts
+++ final-app/src/app/cards/card-form/card-form.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICard } from 'src/app/models/card';
+import { CardService } from '../card.service';
 
 @Component({
   selector: 'app-card-form',
   templateUrl: './card-form.component.html',
   styleUrls: ['./card-form.component.css']
 })
-export class CardFormComponent {
+export class CardFormComponent implements OnInit{
+  cards: ICard[] = []
+  constructor (private cardService: CardService){}
 
+  ngOnInit(): void {
+    this.cardService.getAll().subscribe(cards => {
+      this.cards = cards
+    })
+  }
 }
